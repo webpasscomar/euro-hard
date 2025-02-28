93448fa39e89454a07883d9ea452e75f8b4a52a6
Lean-2021 <leanw1711@gmail.com>
2025-02-28 14:44:26 -0300

    vista

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Product;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ProductRequest extends FormRequest
 {
@@ -22,21 +24,31 @@ class ProductRequest extends FormRequest
   public function rules(): array
   {
     $id = request()->route('product')->id ?? null;
+    $product = $id ? Product::findOrFail($id) : null;
+
     $put = request()->isMethod('put');
 
 
     return [
       'name' => $put ? 'required|string|min:3|max:255|unique:products,name,' . $id : 'required|string|min:3|max:255|unique:products,name',
+      'slug' => [
+        'required',
+        'string',
+        'min:3',
+        'max:100',
+        'regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/',
+        $put ? Rule::unique('products', 'slug')->ignore($id) : Rule::unique('products', 'slug'),
+      ],
       'description' => 'required|min:10|string',
       'image_main' =>  $put ? 'image|mimes:png,jpg,jpeg,svg|max:1024' : 'required|image|mimes:png,jpg,jpeg,svg|max:1024',
       'productCategory_id' => 'required',
       'video' => 'nullable|url',
       'image_1' => 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024',
-      'image_2' => 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024',
-      'image_3' => 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024',
-      'image_4' => 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024',
-      'image_5' => 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024',
-      'image_6' => 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024',
+      'image_2' => $put ? (!request()->hasFile('image_1') && $product->image_1 == null ? 'prohibited' : 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024') : (request()->hasFile('image_1') ? 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024' : 'prohibited'),
+      'image_3' => $put ? (!request()->hasFile('image_2') && $product->image_2 == null ? 'prohibited' : 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024') : (request()->hasFile('image_2') ? 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024' : 'prohibited'),
+      'image_4' => $put ? (!request()->hasFile('image_3') && $product->image_3 == null ? 'prohibited' : 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024') : (request()->hasFile('image_3') ? 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024' : 'prohibited'),
+      'image_5' => $put ? (!request()->hasFile('image_4') && $product->image_4 == null ? 'prohibited' : 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024') : (request()->hasFile('image_4') ? 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024' : 'prohibited'),
+      'image_6' => $put ? (!request()->hasFile('image_5') && $product->image_5 == null ? 'prohibited' : 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024') : (request()->hasFile('image_5') ? 'nullable|image|mimes:png,jpg,jpeg,svg|max:1024' : 'prohibited'),
       'datasheet_file' => 'mimes:pdf|max:2048',
       'instruction_file' => 'mimes:pdf|max:2048',
       'keywordsSEO' => 'max:255',
@@ -52,6 +64,12 @@ class ProductRequest extends FormRequest
       'name.min' => 'Ingrese un mínimo de 3 caracteres',
       'name.max' => 'Máximo 255 caracteres',
       'name.unique' => 'Ya existe un producto con ese nombre',
+      'slug.required' => 'Ingrese un slug',
+      'slug.string' => 'Ingrese un slug válido',
+      'slug.min' => 'Ingrese un mínimo de 3 caracteres',
+      'slug.max' => 'Máximo 100 caracteres',
+      'slug.unique' => 'Ya existe un producto con ese slug',
+      'slug.regex' => 'Ingrese un slug válido',
       'description.required' => 'Ingrese una descripción',
       'description.string' => 'Ingrese una descripción válida',
       'description.min' => 'Ingrese un mínimo de 10 caracteres',
@@ -67,18 +85,23 @@ class ProductRequest extends FormRequest
       'image_2.image' => 'Formato no permitido',
       'image_2.mimes' => 'Formato no permitido',
       'image_2.max' => 'Tamaño no permitido. Máximo 1mb',
+      'image_2.prohibited' => 'Debe subir las imágenes en orden: 1,2,...',
       'image_3.image' => 'Formato no permitido',
       'image_3.mimes' => 'Formato no permitido',
       'image_3.max' => 'Tamaño no permitido. Máximo 1mb',
+      'image_3.prohibited' => 'Debe subir las imágenes en orden: 1,2,...',
       'image_4.image' => 'Formato no permitido',
       'image_4.mimes' => 'Formato no permitido',
       'image_4.max' => 'Tamaño no permitido. Máximo 1mb',
+      'image_4.prohibited' => 'Debe subir las imágenes en orden: 1,2,...',
       'image_5.image' => 'Formato no permitido',
-      'image_5.mimes' => 'Formato no permitido',
+      'image_5.mimes' => 'Formato no permido',
       'image_5.max' => 'Tamaño no permitido. Máximo 1mb',
+      'image_5.prohibited' => 'Debe subir las imágenes en orden: 1,2,...',
       'image_6.image' => 'Formato no permitido',
       'image_6.mimes' => 'Formato no permitido',
       'image_6.max' => 'Tamaño no permitido. Máximo 1mb',
+      'image_6.prohibited' => 'Debe subir las imágenes en orden: 1,2,...',
       'datasheet_file.mimes' => 'Format no permitido. Solo PDF',
       'datasheet_file.max' => 'Tamaño no permitido. Máximo 2mb',
       'instruction_file.mimes' => 'Format no permitido. Solo PDF',
--- resources/views/productos-categorias.blade.php
+++ resources/views/productos-categorias.blade.php
@@ -1,63 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="main-container">
-    <div class="container-hero">
-      <div class="container-fluid">
-        <div class="container-title-hero">
-          <div class="content-icon-EH-hero">
-            <img src="{{ asset('images/icono-EH-gris.svg') }}" class="icon-EH-hero">
-          </div>
-          <h1 class="title-hero">{{ Str::title($categoria->name) }}</h1>
-        </div>
-      </div>
-      <div class="img-hero">
-        <div class="img-overlay"></div>
-        <img
-          src="{{ $categoria->banner && file_exists(public_path('storage/product_categories/' . $categoria->banner)) ? asset('storage/product_categories/' . $categoria->banner) : asset('img/no_disponible.jpg') }}">
-      </div>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <h5 class="title-category gris">
-            Categorías - {{ Str::title($categoria->name) }}
-          </h5>
-        </div>
-      </div>
-      <div class="row">
-        <div class="owl-carousel owl-theme">
-          @foreach ($subcategorias as $subcategoria)
-            @php
-              $imagePath =
-                  $subcategoria->image &&
-                  file_exists(public_path('storage/product_categories/' . $subcategoria->image))
-                      ? asset('storage/product_categories/' . $subcategoria->image)
-                      : asset('img/no_disponible.jpg');
-            @endphp
-            <div class="item item1">
-              <div class="content-cat carrusel-size">
-                <a href="{{route('productos.list', [$categoria->slug, $subcategoria->slug])}}"
-                   class="link-mas-info">
-                  <div class="img-content-subcat" style="background-image: url({{ $imagePath }})">
-                    <div class="title-cat-column">
-                      {{ $subcategoria->name }}
+    <div class="main-container">
+        <div class="container-hero">
+            <div class="container-fluid">
+                <div class="container-title-hero">
+                    <div class="content-icon-EH-hero">
+                        <img src="{{ asset('images/icono-EH-gris.svg') }}" class="icon-EH-hero">
                     </div>
-                  </div>
-                  <div class="footer-content-cat-home">
-                    <div class="cat-capitulo">
-                      <span class="estilo">ESTILO</span> CAP 6
-                    </div>
-                    <div class="btn-mas-info">
-                      + info
-                    </div>
-                  </div>
-                </a>
-              </div>
+                    <h1 class="title-hero">{{ Str::title($categoria->name) }}</h1>
+                </div>
             </div>
-          @endforeach
+            <div class="img-hero">
+                <div class="img-overlay"></div>
+                <img
+                    src="{{ $categoria->banner && file_exists(public_path('storage/product_categories/' . $categoria->banner)) ? asset('storage/product_categories/' . $categoria->banner) : asset('img/no_disponible.jpg') }}">
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h5 class="title-category gris">
+                        Categorías - {{ Str::title($categoria->name) }}
+                    </h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="owl-carousel owl-theme">
+                    @foreach ($subcategorias as $subcategoria)
+                        @php
+                            $imagePath =
+                                $subcategoria->image &&
+                                file_exists(public_path('storage/product_categories/' . $subcategoria->image))
+                                    ? asset('storage/product_categories/' . $subcategoria->image)
+                                    : asset('img/no_disponible.jpg');
+                        @endphp
+                        <div class="item item1">
+                            <div class="content-cat carrusel-size">
+                                <a href="{{ route('productos.list', [$categoria->slug, $subcategoria->slug]) }}"
+                                    class="link-mas-info">
+                                    <div class="img-content-subcat" style="background-image: url({{ $imagePath }})">
+                                        <div class="title-cat-column">
+                                            {{ $subcategoria->name }}
+                                        </div>
+                                    </div>
+                                    <div class="footer-content-cat-home">
+                                        <div class="cat-capitulo">
+                                            <span class="estilo">ESTILO</span> CAP 6
+                                        </div>
+                                        <div class="btn-mas-info">
+                                            + info
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    @endforeach
 
-          {{-- <div class="item item2">
+                    {{-- <div class="item item2">
               <div class="content-cat carrusel-size">
                   <a href="#" class="link-mas-info">
                       <div class="img-content-subcat2">
@@ -77,7 +77,7 @@
               </div>
           </div><!--[fin de Item 2]--> --}}
 
-          {{-- <div class="item item3">
+                    {{-- <div class="item item3">
               <div class="content-cat carrusel-size">
                   <a href="#" class="link-mas-info">
                       <div class="img-content-subcat3">
@@ -97,7 +97,7 @@
               </div>
           </div><!--[fin de Item 3]--> --}}
 
-          {{-- <div class="item item4">
+                    {{-- <div class="item item4">
               <div class="content-cat carrusel-size">
                   <a href="#" class="link-mas-info">
                       <div class="img-content-subcat4">
@@ -117,7 +117,7 @@
               </div>
           </div><!--[fin de Item 4]--> --}}
 
-          {{-- <div class="item item5">
+                    {{-- <div class="item item5">
               <div class="content-cat carrusel-size">
                   <a href="#" class="link-mas-info">
                       <div class="img-content-subcat1">
@@ -137,7 +137,7 @@
               </div>
           </div><!--[fin de Item 5]--> --}}
 
-          {{-- <div class="item item6">
+                    {{-- <div class="item item6">
               <div class="content-cat carrusel-size">
                   <a href="#" class="link-mas-info">
                       <div class="img-content-subcat1">
@@ -157,8 +157,8 @@
               </div>
           </div><!--[fin de Item 6]--> --}}
 
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 @endsection
