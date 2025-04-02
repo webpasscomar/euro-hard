f8c70042b137ccb5df20a61dc177c512c4e307b8
Lean-2021 <leanw1711@gmail.com>
2025-04-02 01:32:47 +0000

    correccion alto de banner ambientes - capitulos

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -4,7 +4,7 @@
 
     <div class="main-container">
 
-        <div class="container-hero">
+        <div class="container-hero" style="height: 320px;">
             <div class="container-fluid">
                 <div class="container-title-hero">
                     <div class="content-icon-EH-hero">
@@ -49,83 +49,81 @@
                 </div>
             </div>
             <div class="row">
-                {{--        comienzo --}}
+                {{-- comienzo --}}
                 @forelse($productos as $producto)
-                    <div class="col-lg-6 p-4">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 p-3 content-product">
-                                @if ($producto->is_new)
-                                    <div class="card-new">
-                                        NUEVO
-                                    </div>
-                                @endif
-                                <div id="carouselProduct{{ $loop->index + 1 }}" class="carousel slide">
-                                    @if (
-                                        !empty($producto->image_1) ||
-                                            !empty($producto->image_2) ||
-                                            !empty($producto->image_3) ||
-                                            !empty($producto->image_4) ||
-                                            !empty($producto->image_5) ||
-                                            !empty($producto->image_6))
-                                        <div class="carousel-indicators">
-                                            <button type="button" data-bs-target="#carouselProduct{{ $loop->index + 1 }}"
-                                                data-bs-slide-to="0" class="active" aria-current="true"
-                                                aria-label="Slide 0">
-                                            </button>
-                                            @for ($i = 1; $i <= 6; $i++)
-                                                @if (!empty($producto->{'image_' . $i}))
-                                                    <button type="button"
-                                                        data-bs-target="#carouselProduct{{ $loop->index + 1 }}"
-                                                        data-bs-slide-to="{{ $i }}" aria-current="true"
-                                                        aria-label="Slide {{ $i }}">
-                                                    </button>
-                                                @endif
-                                            @endfor
-                                        </div>
-                                    @endif
-                                    <div class="carousel-inner carrusel-product">
-                                        <div class="carousel-item active">
-                                            <img src="{{ $producto->image_main && file_exists(public_path('storage/products/' . $producto->image_main)) ? asset('storage/products/' . $producto->image_main) : asset('img/no_disponible.jpg') }}"
-                                                class="d-block w-100" alt="{{ $producto->name }}">
-                                        </div>
-                                        @for ($i = 1; $i <= 6; $i++)
-                                            @if (!empty($producto->{'image_' . $i}))
-                                                <div class="carousel-item">
-                                                    <img src="{{ $producto->{'image_' . $i} && file_exists(public_path('storage/products/' . $producto->{'image_' . $i})) ? asset('storage/products/' . $producto->{'image_' . $i}) : asset('img/no_disponible.jpg') }}"
+                            <div class="col-lg-6 p-4">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 p-3 content-product">
+                                        @if ($producto->is_new)
+                                            <div class="card-new">
+                                                NUEVO
+                                            </div>
+                                        @endif
+                                        <div id="carouselProduct{{ $loop->index + 1 }}" class="carousel slide">
+                                            @if (
+                                                !empty($producto->image_1) ||
+                                                !empty($producto->image_2) ||
+                                                !empty($producto->image_3) ||
+                                                !empty($producto->image_4) ||
+                                                !empty($producto->image_5) ||
+                                                !empty($producto->image_6)
+                                            )
+                                                                        <div class="carousel-indicators">
+                                                                            <button type="button" data-bs-target="#carouselProduct{{ $loop->index + 1 }}"
+                                                                                data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 0">
+                                                                            </button>
+                                                                            @for ($i = 1; $i <= 6; $i++)
+                                                                                @if (!empty($producto->{'image_' . $i}))
+                                                                                    <button type="button" data-bs-target="#carouselProduct{{ $loop->index + 1 }}"
+                                                                                        data-bs-slide-to="{{ $i }}" aria-current="true" aria-label="Slide {{ $i }}">
+                                                                                    </button>
+                                                                                @endif
+                                                                            @endfor
+                                                                        </div>
+                                            @endif
+                                            <div class="carousel-inner carrusel-product">
+                                                <div class="carousel-item active">
+                                                    <img src="{{ $producto->image_main && file_exists(public_path('storage/products/' . $producto->image_main)) ? asset('storage/products/' . $producto->image_main) : asset('img/no_disponible.jpg') }}"
                                                         class="d-block w-100" alt="{{ $producto->name }}">
                                                 </div>
+                                                @for ($i = 1; $i <= 6; $i++)
+                                                    @if (!empty($producto->{'image_' . $i}))
+                                                        <div class="carousel-item">
+                                                            <img src="{{ $producto->{'image_' . $i} && file_exists(public_path('storage/products/' . $producto->{'image_' . $i})) ? asset('storage/products/' . $producto->{'image_' . $i}) : asset('img/no_disponible.jpg') }}"
+                                                                class="d-block w-100" alt="{{ $producto->name }}">
+                                                        </div>
+                                                    @endif
+                                                @endfor
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 p-3 content-product">
+                                        <div class="title-product">
+                                            {{ Str::ucfirst($producto->name) }}
+                                        </div>
+                                        <div class="description-product">
+                                            {{ Str::ucfirst($producto->description) }}
+                                        </div>
+                                        <div class="content-btn-products">
+                                            @isset($categoria)
+                                                <a class="btn-rojo"
+                                                    href="{{ route('productos.detalle', [$categoria, $subcategoria, $producto]) }}">Ficha
+                                                    técnica</a>
+                                            @else
+                                                <a class="btn-rojo"
+                                                    href="{{ route('productos.detalle', ['categoria', $subcategoria, $producto]) }}">Ficha
+                                                    técnica</a>
+                                            @endisset
+                                            @if ($producto->video)
+                                                <button type="button" class="btn-gris" data-bs-toggle="modal"
+                                                    data-bs-target="#modalVideo{{ $producto->id }}">Video
+                                                </button>
+                                                <x-modal-youtube :id="$producto->id" :video-url="$producto->video" />
                                             @endif
-                                        @endfor
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6 col-md-6 p-3 content-product">
-                                <div class="title-product">
-                                    {{ Str::ucfirst($producto->name) }}
-                                </div>
-                                <div class="description-product">
-                                    {{ Str::ucfirst($producto->description) }}
-                                </div>
-                                <div class="content-btn-products">
-                                    @isset($categoria)
-                                        <a class="btn-rojo"
-                                            href="{{ route('productos.detalle', [$categoria, $subcategoria, $producto]) }}">Ficha
-                                            técnica</a>
-                                    @else
-                                        <a class="btn-rojo"
-                                            href="{{ route('productos.detalle', ['categoria', $subcategoria, $producto]) }}">Ficha
-                                            técnica</a>
-                                    @endisset
-                                    @if ($producto->video)
-                                        <button type="button" class="btn-gris" data-bs-toggle="modal"
-                                            data-bs-target="#modalVideo{{ $producto->id }}">Video
-                                        </button>
-                                        <x-modal-youtube :id="$producto->id" :video-url="$producto->video" />
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 @empty
                     <div class="row">
                         <div class="col">
