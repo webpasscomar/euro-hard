512bd82564d1675d5257dca3bf4e7df92e103fef
Eduardo Passarelli <epassarelli@gmail.com>
2025-04-30 15:45:43 -0300

    Se agrega Iframe YT hardcodeado para probar en el modal

--- resources/views/components/modal-youtube.blade.php
+++ resources/views/components/modal-youtube.blade.php
@@ -1,24 +1,28 @@
 <div>
-    <!-- Modal -->
-    <div class="modal fade" id="{{ 'modalVideo' . $id }}" tabindex="-1" aria-labelledby="modalYoutubeTitle"
-        aria-hidden="true" style="background: rgba(61,60,60,0.8);">
-        <div class="modal-dialog modal-lg modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header" style="background-color: #3a3838; height: 60px;">
-                    <h1 class="modal-title" id="modalYoutubeTitle"><img
-                            src="{{ asset('images/logo-eurohard-white.png') }}" alt=""
-                            class="img-fluid w-75 mb-3">
-                    </h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
-                        style="background-color:white;"></button>
-                </div>
-                <div class="modal-body ratio ratio-16x9">
-                    <iframe class="" src="{{ $videoUrl }}" title="YouTube video player" frameborder="0"
+  <!-- Modal -->
+  <div class="modal fade" id="{{ 'modalVideo' . $id }}" tabindex="-1" aria-labelledby="modalYoutubeTitle" aria-hidden="true"
+    style="background: rgba(61,60,60,0.8);">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header" style="background-color: #3a3838; height: 60px;">
+          <h1 class="modal-title" id="modalYoutubeTitle"><img src="{{ asset('images/logo-eurohard-white.png') }}"
+              alt="" class="img-fluid w-75 mb-3">
+          </h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+            style="background-color:white;"></button>
+        </div>
+        <div class="modal-body ratio ratio-16x9">
+          {{-- <iframe class="" src="{{ $videoUrl }}" title="YouTube video player" frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </iframe>
-                </div>
-            </div>
+                    </iframe> --}}
+
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/E99jMsE21DA?si=6hFbjzBnmDwXK6Ri"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
+      </div>
     </div>
+  </div>
 </div>
